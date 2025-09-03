ba13bfcca40ce9530e462d2bab09bbff320f125f
Gwill1337 <gwyllig@gmail.com>
2025-09-03 22:57:55 +0300

    Update ServiceControl.psm1

--- AdminToolsModules/Modules/ServiceControl.psm1
+++ AdminToolsModules/Modules/ServiceControl.psm1
@@ -1,4 +1,39 @@
 function Start-AdminService {
+    <#
+    .SYNOPSIS
+        Starts a Windows service with additional administrative features
+    
+    .DESCRIPTION
+        Starts the specified Windows service. If the service is already running and -Force is specified,
+        restarts the service. Includes logging and error handling.
+    
+    .PARAMETER Name
+        Specifies the service name. This parameter is mandatory.
+    
+    .PARAMETER Force
+        Forces the service to restart if it is already running
+    
+    .EXAMPLE
+        Start-AdminService -Name "Spooler"
+        
+        Starts the Print Spooler service
+    
+    .EXAMPLE
+        Start-AdminService -Name "WinRM" -Force
+        
+        Restarts the Windows Remote Management service even if it's running
+    
+    .INPUTS
+        String. You can pipe service names to this function.
+    
+    .OUTPUTS
+        None. Writes output to console and log.
+    
+    .NOTES
+        Author: Gwill1337
+        Requires: Administrator privileges
+        Version: 1.0.0
+    #>
     param (
 
         [Parameter(Mandatory = $true)]
@@ -34,6 +69,40 @@ function Start-AdminService {
 
 
 function Stop-AdminService {
+    <#
+    .SYNOPSIS
+        Stops a Windows service with administrative features
+    
+    .DESCRIPTION
+        Stops the specified Windows service. Includes logging and error handling.
+    
+    .PARAMETER Name
+        Specifies the service name. This parameter is mandatory.
+    
+    .PARAMETER Force
+        Forces the service to stop even if it has dependent services
+    
+    .EXAMPLE
+        Stop-AdminService -Name "SomeService"
+        
+        Stops the specified service
+    
+    .EXAMPLE
+        Stop-AdminService -Name "AnotherService" -Force
+        
+        Forcefully stops the service
+    
+    .INPUTS
+        String. You can pipe service names to this function.
+    
+    .OUTPUTS
+        None. Writes output to console and log.
+    
+    .NOTES
+        Author: Gwill1337
+        Requires: Administrator privileges
+        Version: 1.0.0
+    #>
     param (
         [Parameter(Mandatory = $true)]
         [string]$Name,
@@ -65,6 +134,41 @@ function Stop-AdminService {
 }
 
 function Restart-AdminService {
+    <#
+    .SYNOPSIS
+        Restarts a Windows service with administrative features
+    
+    .DESCRIPTION
+        Restarts the specified Windows service. If the service is stopped, starts it.
+        Includes logging and error handling.
+    
+    .PARAMETER Name
+        Specifies the service name. This parameter is mandatory.
+    
+    .PARAMETER Force
+        Forces the service to restart even if it has dependent services
+    
+    .EXAMPLE
+        Restart-AdminService -Name "Spooler"
+        
+        Restarts the Print Spooler service
+    
+    .EXAMPLE
+        Restart-AdminService -Name "WinRM" -Force
+        
+        Forcefully restarts the Windows Remote Management service
+    
+    .INPUTS
+        String. You can pipe service names to this function.
+    
+    .OUTPUTS
+        None. Writes output to console and log.
+    
+    .NOTES
+        Author: Gwill1337
+        Requires: Administrator privileges
+        Version: 1.0.0
+    #>
     param (
         [Parameter(Mandatory = $true)]
         [string]$Name,
@@ -99,6 +203,49 @@ function Restart-AdminService {
 }
 
 function Watch-AdminService {
+    <#
+    .SYNOPSIS
+        Monitors a Windows service and automatically restarts it if it stops
+    
+    .DESCRIPTION
+        Sets up event-based monitoring for a Windows service. If the service stops,
+        it will be automatically restarted. Includes force option and stop monitoring capability.
+    
+    .PARAMETER Name
+        Specifies the service name to monitor. This parameter is mandatory.
+    
+    .PARAMETER Force
+        Forces the service restart when using automatic recovery
+    
+    .PARAMETER StopWatch
+        Stops the monitoring for the specified service
+    
+    .EXAMPLE
+        Watch-AdminService -Name "CriticalService"
+        
+        Starts monitoring the critical service
+    
+    .EXAMPLE
+        Watch-AdminService -Name "ImportantService" -Force
+        
+        Starts monitoring with forceful restart
+    
+    .EXAMPLE
+        Watch-AdminService -Name "SomeService" -StopWatch
+        
+        Stops monitoring the service
+    
+    .INPUTS
+        String. You can pipe service names to this function.
+    
+    .OUTPUTS
+        None. Sets up event monitoring in the background.
+    
+    .NOTES
+        Author: Gwill1337
+        Requires: Administrator privileges
+        Version: 1.0.0
+    #>
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $true)]
