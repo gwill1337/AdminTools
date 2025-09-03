45728f20a7e1bff8473cac43c70e1bded558e876
Gwill1337 <gwyllig@gmail.com>
2025-09-03 22:57:44 +0300

    Update SecurityLogs.psm1

--- AdminToolsModules/Modules/SecurityLogs.psm1
+++ AdminToolsModules/Modules/SecurityLogs.psm1
@@ -1,4 +1,45 @@
 function Get-AdminEvent {
+    <#
+    .SYNOPSIS
+        Gets Windows event log entries with filtering options
+    
+    .DESCRIPTION
+        Retrieves events from Windows security log with advanced filtering capabilities.
+        Supports filtering by user, event ID, time range, and failed logon events.
+    
+    .PARAMETER UserName
+        Filters events by username
+    
+    .PARAMETER LastDays
+        Number of days to look back. Default is 1 day.
+    
+    .PARAMETER EventID
+        Filters by specific event IDs
+    
+    .PARAMETER MaxLenMessage
+        Maximum length of message to return. Default is 250 characters.
+    
+    .PARAMETER FailedLogon
+        Filters for failed logon events (Event ID 4625)
+    
+    .EXAMPLE
+        Get-AdminEvent -LastDays 7
+        
+        Returns events from last 7 days
+    
+    .EXAMPLE
+        Get-AdminEvent -UserName "john.doe" -EventID 4625 -FailedLogon
+        
+        Returns failed logon events for specific user
+    
+    .OUTPUTS
+        PSCustomObject. Returns event log entries.
+    
+    .NOTES
+        Author: Gwill1337
+        Requires: Read access to security log
+        Version: 1.0.0
+    #>
     [CmdletBinding()]
     param (
         [string]$UserName,
@@ -58,6 +99,43 @@ function Get-AdminEvent {
 
 
 function Get-AdminInstalledUpdates {
+    <#
+    .SYNOPSIS
+        Gets information about installed Windows updates
+    
+    .DESCRIPTION
+        Retrieves information about installed hotfixes and updates.
+        Supports filtering by date, update type, and computer name.
+    
+    .PARAMETER SinceDate
+        Filters updates installed since specified date
+    
+    .PARAMETER Type
+        Filters by update type (e.g., "Security Update", "Update")
+    
+    .PARAMETER ComputerName
+        Gets updates from remote computers
+    
+    .PARAMETER SortByDate
+        Sorts results by installation date
+    
+    .EXAMPLE
+        Get-AdminInstalledUpdates -SinceDate (Get-Date).AddDays(-30)
+        
+        Returns updates installed in last 30 days
+    
+    .EXAMPLE
+        Get-AdminInstalledUpdates -Type "Security Update" -SortByDate
+        
+        Returns security updates sorted by date
+    
+    .OUTPUTS
+        PSCustomObject. Returns update information.
+    
+    .NOTES
+        Author: Gwill1337
+        Version: 1.0.0
+    #>
     param (
         [datetime]$SinceDate,
         [string]$Type,
